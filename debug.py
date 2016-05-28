1ba11b82fe59288ae0108ea1b0b34ab8203cb8d4
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-05-28 12:52:01 +0530

    Added dump_midi functionality

---
+++ debug.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+import midi
+
+
+pattern = midi.Pattern()
+track = midi.Track()
+pattern.append(track)
+
+
+track.append(midi.NoteOnEvent(tick=0, channel=0, data=[48, 105]))
+track.append(midi.NoteOnEvent(tick=0, channel=0, data=[60, 105]))
+track.append(midi.NoteOffEvent(tick=2, channel=0, data=[60, 0]))
+track.append(midi.NoteOnEvent(tick=1, channel=0, data=[60, 80]))
+track.append(midi.NoteOffEvent(tick=2, channel=0, data=[60, 0]))
+track.append(midi.NoteOnEvent(tick=1, channel=0, data=[60, 95]))
+track.append(midi.NoteOffEvent(tick=2, channel=0, data=[60, 0]))
+track.append(midi.NoteOnEvent(tick=1, channel=0, data=[60, 80]))
+track.append(midi.NoteOffEvent(tick=2, channel=0, data=[48, 0]))
+track.append(midi.NoteOffEvent(tick=0, channel=0, data=[60, 0]))
+track.append(midi.NoteOnEvent(tick=1, channel=0, data=[48, 95]))
+track.append(midi.NoteOnEvent(tick=0, channel=0, data=[65, 95]))
+track.append(midi.NoteOffEvent(tick=2, channel=0, data=[65, 0]))
+
+
+eot = midi.EndOfTrackEvent(tick=1)
+track.append(eot)
+
+
+print pattern
+midi.write_midifile("debug.mid", pattern)
--- process_midi.py
+++ process_midi.py
@@ -13,9 +13,9 @@ def compress_state(state):
 
 
 def compress_state_matrix(state_matrix):
-    ret = []
-    for state in state_matrix:
-        ret.append(compress_state(state))
+    ret = ""
+    for idx, state in enumerate(state_matrix):
+        ret += str(idx) + ".) " + str(compress_state(state)) + "\n"
     return ret
 
 
@@ -40,10 +40,71 @@ def load_midi(filepath):
     return state_matrix
 
 
+def get_next_different_state(state_matrix, index):
+    for i in xrange(index + 1, len(state_matrix)):
+        # print 'Inside, ', i
+        if state_matrix[i] != state_matrix[index]:
+            return i
+    return len(state_matrix)
+
+
+def state_diff(current_state, next_state):
+    notes_on = []
+    notes_off = []
+
+    for pitch, (volume1, volume2) in enumerate(zip(current_state, next_state)):
+        if volume1 == 0 and volume2 > 0:
+            notes_on.append([pitch, volume2])
+        elif volume1 > 0 and volume2 == 0:
+            notes_off.append([pitch, volume2])
+
+    return (notes_on, notes_off)
+
+
+def dump_midi(state_matrix, filepath):
+    pattern = midi.Pattern()
+    track = midi.Track()
+    pattern.append(track)
+
+    # Append the very first tick
+    notes_on, _ = state_diff([0] * 128, state_matrix[0])
+    for note in notes_on:
+        track.append(midi.NoteOnEvent(tick=0, channel=0, data=note))
+
+    # Append the rest of the ticks
+    current_state_index = 0
+    while current_state_index < len(state_matrix):
+        next_state_index = get_next_different_state(
+            state_matrix, current_state_index)
+        ticks_elapsed = next_state_index - current_state_index
+
+        current_state = state_matrix[current_state_index]
+        next_state = state_matrix[next_state_index] if next_state_index < len(
+            state_matrix) else [0] * 128
+        notes_on, notes_off = state_diff(current_state, next_state)
+
+        for note in notes_on:
+            track.append(midi.NoteOnEvent(
+                tick=ticks_elapsed, channel=0, data=note))
+        for note in notes_off:
+            track.append(midi.NoteOffEvent(
+                tick=ticks_elapsed, channel=0, data=note))
+
+        current_state_index = next_state_index
+
+    track.append(midi.EndOfTrackEvent(tick=1))
+    midi.write_midifile(filepath, pattern)
+
+    return pattern
+
+
 def main():
-    filepath = 'music/e1.midi'
+    filepath = 'music/debug.mid'
     state_matrix = load_midi(filepath)
-    pprint(compress_state_matrix(state_matrix))
+    print compress_state_matrix(state_matrix)
+    print '\n' * 5
+    pattern = dump_midi(state_matrix, 'out.mid')
+    pprint(pattern)
 
 
 if __name__ == '__main__':
